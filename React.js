dafa4de2b194ee02aeb57de9a4849d2d4fe6a316
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2025-06-16 11:01:27 +0530

    Create React.js

---
+++ React.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Smart from './smart.js'; 
+function App() {
+  return (
+    <div className="App">
+      {<header className="App-header">
+        <img src={"smarthome.png"} className="App-logo" alt="logo" />
+        <p>
+          SMART HOME TECHNOLOGY 
+        </p>
+        <a
+          className="App-link"
+          href="https://images.app.goo.gl/m3LFtKsVDmwbYTXo8"
+          rel="noopener noreferrer"
+        >
+          SMART HOME
+        </a>
+        <Sviet title="Smart home"/>
+        </header> 
+        }
+    </div>
+  );
+}
+export default App;
